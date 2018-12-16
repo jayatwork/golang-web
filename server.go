05d7622108ad92699cddad276c3c0ed78e894792
Ancrum, Jason <jason.ancrum00@delta.com>
2018-12-16 14:31:16 -0500

    enriched web with increment function

--- server.go
+++ server.go
@@ -2,16 +2,31 @@ package main
 
 import (
 	"fmt"
-	"html"
 	"log"
 	"net/http"
+	"strconv"
+	"sync"
 )
+
+var counter int
+var mutex = &sync.Mutex{}
+
+func echoString("/", func(w http.ResponseWriter, r *http.Request) {
+	        fmt.Fprintf(w, "Hello")
+	}
+	
+func incrementCounter(w http.ResponseWriter, r *http.Request) {
+	mutex.Lock()
+	counter++
+	fmt.Fprintf(w, strconv.Itoa(counter))
+	mutex.Unlock()
+}
 
 func main() {
 
-	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
-		fmt.Fprintf(w, "Hello, %q", html.EscapeString(r.URL.Path))
-	})
+	http.HandleFunc("/", echoString) 
+
+	http.HandleFunc("/increment", incrementCounter)
 
 	http.HandleFunc("/hi", func(w http.ResponseWriter, r *http.Request) {
 		fmt.Fprintf(w, "Hi")
